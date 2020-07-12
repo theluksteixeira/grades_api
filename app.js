ad48f17598bc2102cf2584a3496675eaaabcc89f
Lucas Teixeira <sobrinho1@gmail.com>
2020-07-12 11:35:14 -0300

    mehorias

--- app.js
+++ app.js
@@ -26,9 +26,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  cors({
-    origin: 'http://localhost:8080',
-  })
+  cors()
 );
 
 app.use(gradeRouter);
